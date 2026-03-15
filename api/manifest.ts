fa6c2a99c3a6abe36839425e3bcc33536f985449
Le Hoang Anh <anh.le@angelhack.com>
2026-03-15 22:47:51 +0700

    WIP: update manifest.json

--- api/manifest.ts
+++ api/manifest.ts
@@ -9,10 +9,7 @@ const manifest = {
   theme_color: "#2563eb",
   background_color: "#ffffff",
   icons: [
-    { src: "/IHN.png", sizes: "192x192", type: "image/png", purpose: "any" as const },
-    { src: "/IHN.png", sizes: "256x256", type: "image/png", purpose: "any" as const },
-    { src: "/IHN.png", sizes: "384x384", type: "image/png", purpose: "any" as const },
-    { src: "/IHN.png", sizes: "512x512", type: "image/png", purpose: "any" as const },
+    { src: "/IHN.png", sizes: "1280x1280", type: "image/png", purpose: "any" as const },
   ],
   id: "/?homescreen=1",
 };
