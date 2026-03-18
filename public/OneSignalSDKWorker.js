1710dd3fdb1950af917ced330503cf66c2abd872
Le Hoang Anh <anh.le@angelhack.com>
2026-03-18 11:01:51 +0700

    chore: rewrite OneSignalSDKWorker path to handle the COOP issue; correct MIME type

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,2 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+// Load from same origin to avoid ad blockers blocking cdn.onesignal.com
+importScripts("/OneSignalSDK.sw.js");
